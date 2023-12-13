ed6d2725620b66c1e3a00b9e03261eb5ae93d7dc
joexy <josephethethowo@gmail.com>
2023-12-13 16:37:05 +0100

    add footer component

---
+++ src/components/layouts/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    const footerYear = new Date().getFullYear()
+  return (
+    <footer className="footer p-10 bg-gray-700 text-primary-content footer-center">
+        <p>&copy; {footerYear} - All rights reserved</p>
+    </footer>
+  )
+}
+export default Footer
