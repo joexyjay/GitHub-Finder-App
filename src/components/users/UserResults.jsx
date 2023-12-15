a0f6148091cf495bab01cf144f2cc95e29fd0542
joexy <josephethethowo@gmail.com>
2023-12-15 15:10:41 +0100

    add users display

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import UserItem from './UserItem'
 import Spinner from '../layouts/Spinner'
 function UserResults() {
 
@@ -24,7 +25,7 @@ function UserResults() {
     return (
        loading ? <Spinner /> : ( <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
        {users.map((user) => (
-           <h3 key={user.id}>{user.login}</h3>
+           <UserItem key={user.id} user={user}/>
        ))}
    </div>)
     )
