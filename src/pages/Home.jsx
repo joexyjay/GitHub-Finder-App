9b56827a7a41c6e6e200363554a0a5ab7fb22670
joexy <josephethethowo@gmail.com>
2024-01-08 16:21:06 +0100

    spelling correction

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import UserResults from "../components/users/userResults"
+import UserResults from "../components/users/UserResults"
 import UserSearch from "../components/users/UserSearch"
 function Home() {
   return (
